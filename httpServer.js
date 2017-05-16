75ff37d83b50cedd0f8ab5fc9f87e9b4382b4909
unknown <guminji@zhelihao.com>
2017-05-16 18:23:19 +0800

    update

--- httpServer.js
+++ httpServer.js
@@ -6,25 +6,16 @@ var socket = require("socket.io");
 var ioRouter =require("./ioRouter");
 var fs = require('fs');
 app.use('/',function(req,res){
-    //io.emit('connection');
     fs.readFile(__dirname + '/resource/html/socket.html', {encoding: 'utf8'}, function (err, data) {
         if(err) {
             console.error(err);
             return;
         }
-        //console.log(data);
         res.end(data);
     });
-    ;
-    //res.end('hellp world!');
-
 })
 var http = require('http');
 var httpServer = http.Server(app);
 var io = socket(httpServer);
-io.on('connection', function(socket){
-    console.log('a user connected');
-});
-
-//ioRouter(io);
+ioRouter(io);
 module.exports = httpServer;
--- ioRouter.js
+++ ioRouter.js
@@ -1,9 +1,11 @@
 /**
  * Created by guminji on 2017/5/16.
  */
+var i =0;
 var ioRouter = function(io){
     io.on("connection",function(){
-        console.log('i am a user');
+        console.log('i am a user'+i);
+        i=i+1;
     })
 }
 module.exports = ioRouter;
