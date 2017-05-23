37a6f7252abca2eafe1f245264be4e12e71d8079
unknown <guminji@zhelihao.com>
2017-05-23 18:50:53 +0800

    update

--- ioRouter.js
+++ ioRouter.js
@@ -3,7 +3,12 @@
  */
 var i =0;
 var ioRouter = function(io){
-    io.on("connection",function(){
+    io.on("connection",function(socket){
+        socket.on('disconnect',function(){
+            console.log('one user disconnect!!');
+            //socket.emit('net work error','hello world!')
+        })
+        socket.emit('net work error','hello world!')
         console.log('i am a user'+i);
         i=i+1;
     })
